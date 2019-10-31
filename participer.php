31b5dba3a4ddb05bf3e051a34b34fe108c06d8b7
Chris S <cs.simon@live.fr>
2019-10-31 11:37:49 +0100

    secondcommit

--- participer.php
+++ participer.php
@@ -1,3 +1,7 @@
-<?php 
-    echo $_POST['email'];
+<?php
+    if (isset($_POST['email']) AND isset($_POST['nom']) AND isset($_POST['prenom'])){
+    echo htmlspecialchars($_POST['email']);
+    echo htmlspecialchars($_POST['nom']);
+    echo htmlspecialchars($_POST['prenom']);
+} 
 ?>
