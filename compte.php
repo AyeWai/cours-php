e1e5b381812688373f8ea41139013112ed555dfa
Chris S <cs.simon@live.fr>
2019-10-31 12:26:17 +0100

    sixthcommit

---
+++ compte.php
@@ -0,0 +1,9 @@
+<?php
+
+if(isset($_POST['login']) AND isset($_POST['password']) AND $_POST['login'] == 'admin' && $_POST['password'] == "hello"){
+    echo "« Bonjour Admin, ceci est votre tableau de bord »";
+}
+else{
+    
+}
+?>
--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 
 $formulaire = '<form action="compte.php" method="post" >
 <p>
-        <input type="input" name="email" placeholder="Email"/><br />
-        <input type="password" name="nom" placeholder="Mot de passe"/><br />
+        <input type="input" name="login" placeholder="Email"/><br />
+        <input type="password" name="password" placeholder="Mot de passe"/><br />
         <input type="submit" value="Connexion" />
 </p>
 </form>';
