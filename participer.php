fa115505316bd2dfc69696f820625b7c62a876d6
Chris S <cs.simon@live.fr>
2019-10-31 11:50:20 +0100

    thirdcommit

--- participer.php
+++ participer.php
@@ -1,7 +1,14 @@
 <?php
     if (isset($_POST['email']) AND isset($_POST['nom']) AND isset($_POST['prenom'])){
-    echo htmlspecialchars($_POST['email']);
-    echo htmlspecialchars($_POST['nom']);
-    echo htmlspecialchars($_POST['prenom']);
+    echo htmlspecialchars($_POST['email'])." ";
+    echo htmlspecialchars($_POST['nom'])." ";
+    echo htmlspecialchars($_POST['prenom'])." ";
+
+    $mailvar = strip_tags($_POST['email']);
+    $namevar = strip_tags($_POST['nom']);
+    $fnamevar = strip_tags($_POST['prenom']);
+
+    $f = fopen("$mailvar.txt","w");
+    fwrite($f,"$mailvar, $namevar, $fnamevar");
 } 
 ?>
