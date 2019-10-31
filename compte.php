e3d4c8d54cbe3fb691520af4eb9f78bdef1174bb
Chris S <cs.simon@live.fr>
2019-10-31 14:08:23 +0100

    seventhcommit

--- compte.php
+++ compte.php
@@ -1,9 +1,10 @@
 <?php
 
-if(isset($_POST['login']) AND isset($_POST['password']) AND $_POST['login'] == 'admin' && $_POST['password'] == "hello"){
-    echo "« Bonjour Admin, ceci est votre tableau de bord »";
+if(isset($_POST['login']) AND isset($_POST['password']) AND $_POST['login'] == "admin" && $_POST['password'] == "hello"){
+    echo "Bonjour Admin, ceci est votre tableau de bord";
 }
 else{
-    
+
+    header("login.php");
 }
 ?>
